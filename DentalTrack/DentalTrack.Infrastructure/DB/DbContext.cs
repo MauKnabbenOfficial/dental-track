55346df4e164685879f9623972e769fdf26c6517
MauKnabbenOfficial <knabben.mau@gmail.com>
2025-10-24 22:29:05 -0300

    plus

---
+++ DentalTrack/DentalTrack.Infrastructure/DB/DbContext.cs
@@ -0,0 +1,23 @@
+﻿using DentalTrack.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DentalTrack.Infrastructure.DB
+{
+    public class DbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public DbContext(DbContextOptions<DbContext> options) : base(options) { }
+
+        // Exemplos:
+        public DbSet<Usuario> Usuarios => Set<Usuario>();
+        //public DbSet<Paciente> Pacientes => Set<Paciente>();
+        //public DbSet<Cirurgia> Cirurgias => Set<Cirurgia>();
+        //public DbSet<Procedimento> Procedimentos => Set<Procedimento>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(DbContext).Assembly);
+        }
+    }
+}
---
+++ DentalTrack/DentalTrack.Infrastructure/DB/DesignTimeDbContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace DentalTrack.Infrastructure.DB
+{
+    //public sealed class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<DbContext>
+    //{
+    //    //public DbContext CreateDbContext(string[] args)
+    //    //{
+    //    //    var config = new ConfigurationBuilder()
+    //    //        .AddJsonFile("appsettings.Development.json", optional: true)
+    //    //        .AddEnvironmentVariables()
+    //    //        .Build();
+
+    //    //    var teste = IConfigurationBuilder
+    //    //        .AddJsonFile("appsettings.json", optional: true)
+    //    //        .AddEnvironmentVariables()
+    //    //        .Build();
+
+    //    //    var conn = config.GetConnectionString("DentalTrackDb")
+    //    //               ?? Environment.GetEnvironmentVariable("DENTALTRACK_CONN");
+
+    //    //    var options = new DbContextOptionsBuilder<DbContext>()
+    //    //        .UseSqlServer(conn).Options;
+
+    //    //    return new DbContext(options);
+    //    //}
+    //}
+}
